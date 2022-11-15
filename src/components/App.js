d56635a8c3d0ad374f8dd58dac38ed7617049247
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-15 10:08:57 -0300

    fetching data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const resp = await youtube.get("/search", {
+      params: { q: term },
+    });
+    this.setState({ videos: resp.data.items });
+  };
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
-        {/* <VideoDetail/> <VideoList/>  */}
+        <SearchBar handleSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
