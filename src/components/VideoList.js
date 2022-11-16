947c8e85c34be8de72f9ed0bec08d5bea04b59a9
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-15 21:03:21 -0300

    VideoList component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,14 @@
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const showVideo = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return (
+      <VideoItem
+        video={video}
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+      />
+    );
   });
   return <div className="ui relaxed divided list">{showVideo}</div>;
 };
